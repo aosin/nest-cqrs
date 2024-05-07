73e927ecac1784fddf8de8a697597122a3528d76
Aleksei Osin <aleksei_osin@epam.com>
2024-05-07 16:31:15 +0200

    Initial commit  * + swagger  * + sqlite / sequelize

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('CQRS demo')
+    .setDescription("A simple 'hello world' app demoing CQRS")
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+
+bootstrap();
